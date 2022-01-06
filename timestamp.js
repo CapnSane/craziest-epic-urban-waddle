12b4c2607fa2d7fb713566f2087c97e19b7db4a1
Leonardo Schneider <lnrdschneider@gmail.com>
2022-01-06 16:48:42 -0300

    timestamp javascript

--- timestamp.js
+++ timestamp.js
@@ -1,37 +1,5 @@
-// let timestamp = 637776829355875500 % 10 ** 8;
-// let timestamp = 637776829355875500;
-// console.log(timestamp);
-
-// function timeConverter(timestamp) {
-//   var a = new Date(timestamp * 1000);
-//   var months = [
-//     'Jan',
-//     'Feb',
-//     'Mar',
-//     'Apr',
-//     'May',
-//     'Jun',
-//     'Jul',
-//     'Aug',
-//     'Sep',
-//     'Oct',
-//     'Nov',
-//     'Dec'
-//   ];
-//   var year = a.getFullYear();
-//   var month = months[a.getMonth()];
-//   var date = a.getDate();
-//   var hour = a.getHours();
-//   var min = a.getMinutes();
-//   var sec = a.getSeconds();
-//   var time =
-//     date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec;
-//   return time;
-// }
-
-// console.log(timeConverter(timestamp));
-let timestamp = 637776829355875500 / 10 ** 6;
-console.log(timestamp);
+let timestamp = Math.floor(637776829355875500 / 10 ** 6);
+console.log('timestamp = ', timestamp);
 var date = new Date(timestamp);
-console.log(date.getTime());
-console.log(date);
+console.log('date.getTime() = ', date.getTime());
+console.log('date = ', date);
