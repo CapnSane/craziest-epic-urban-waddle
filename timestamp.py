6e8d9e6dd718b2cf10cde019c9dc8eff8b75b81d
Leonardo Schneider <lnrdschneider@gmail.com>
2022-01-06 17:51:36 -0300

    convert timestamp to int

--- timestamp.py
+++ timestamp.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 
-timestamp = 637776829355875500 / 10**9
+timestamp = int(637776829355875500 / 10**9)
+print("timestamp", timestamp)
 dt_object = datetime.fromtimestamp(timestamp)
 
 print("dt_object =", dt_object)
 print("type(dt_object) =", type(dt_object))
-
